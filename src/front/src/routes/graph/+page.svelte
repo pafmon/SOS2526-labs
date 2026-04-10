775d3e7acefe60a64592f420b5b994397b88fe87
pablofm <pablofm@us.es>
2026-04-10 12:12:27 +0200

    l10

--- src/front/src/routes/graph/+page.svelte
+++ src/front/src/routes/graph/+page.svelte
@@ -2,20 +2,18 @@
     import Highcharts from 'highcharts';
     import { onMount } from 'svelte';
 
+    let myData = $state([1,2,3,4,5,6]);
+
     onMount(async () => {
+
+      const res = await fetch("/api/v1/data");
+
+      myData = await res.json();
+
       Highcharts.chart('container', {
         series: [{
-            name: 'Installation & Developers',
-            data: [
-                43934, 48656, 65165, 81827, 112143, 142383,
-                171533, 165174, 155157, 161454, 154610, 168960, 171558
-            ]
-        }, {
-            name: 'Manufacturing',
-            data: [
-                24916, 37941, 29742, 29851, 32490, 30282,
-                38121, 36885, 33726, 34243, 31050, 33099, 33473
-            ]
+            name: 'My Data',
+            data: myData,
         }]
       });
 
